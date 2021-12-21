19ae4579b0d9be203c473059a951163a4b07d0e1
yamaslena@yandex.ru <yamaslena@yandex.ru>
2021-12-21 11:19:04 +0700

    + SmartView, updateData, updateElement

---
+++ src/view/smart-view.js
@@ -0,0 +1,30 @@
+import AbstractView from './abstract-view.js';
+
+export default class SmartView extends AbstractView {
+  _data = {}
+
+  // 6.1.3 Обучит компонент обновлять данные
+  updateData = (update) => {
+    if (!update) {
+      return;
+    }
+
+    this._data = { ...this._data, ...update };
+
+    this.updateElement();
+  }
+
+  // 6.1.2 Обучит компонент обновлять шаблон
+  updateElement = () => {
+    const prevElement = this.element;
+    const parent = prevElement.parentElement;
+    this.removeElement();
+
+    const newElement = this.element;
+
+    parent.replaceChild(newElement, prevElement);
+
+    this.restoreHandlers();
+  }
+
+}
