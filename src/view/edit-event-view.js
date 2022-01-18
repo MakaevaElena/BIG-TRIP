820de323c60158630e46a180fa59b59d04e4d380
yamaslena@yandex.ru <yamaslena@yandex.ru>
2022-01-18 10:20:33 +0700

    скрыть offers если пусто

--- src/view/edit-event-view.js
+++ src/view/edit-event-view.js
@@ -80,10 +80,9 @@ const createEditEventTemplate = (data, possibleDestinations) => {
     isDeleting
   } = data;
 
-
   //!
   let isOffer = '';
-  if (!offers.length === 0) {
+  if (offers.length === 0) {
     isOffer = 'visually-hidden';
   }
 
@@ -150,7 +149,7 @@ const createEditEventTemplate = (data, possibleDestinations) => {
       </button>
     </header>
     <section class="event__details">
-      <section class="event__section  event__section--offers" ${isOffer}>
+      <section class="event__section  event__section--offers  ${isOffer}">
         <h3 class="event__section-title  event__section-title--offers">Offers</h3>
 
         <div class="event__available-offers">
